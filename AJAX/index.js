b26ebd4f1bf869c87733a266a590a589f39863fe
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2022-05-09 05:35:59 -0300

    Curso CSS

---
+++ AJAX/index.js
@@ -0,0 +1,16 @@
+let inputCep = document.querySelector(".input-cep");
+let cep = inputCep.value.replace('-', '');
+console.log(cep);
+// let url = 'http://viacep.com.br/ws/'+ cep + '/json';
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', url, true);
+// xhr.onreadystatechange = function () {
+// if(xhr.readyState == 4) {
+//     if(xhr.status = 200) {
+//         console.log(xhr.responseText);
+//     }
+//   }
+//   xhr.send();
+// }
+
+
