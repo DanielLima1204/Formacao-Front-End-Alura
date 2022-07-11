6af8866f6e7421b91f51ecb11c02d7df25ac0970
Daniel Lima <91384776+DanielLima1204@users.noreply.github.com>
2022-07-11 02:00:26 -0300

    Funções anônimas

--- JS - Para Web/main.js
+++ JS - Para Web/main.js
@@ -1,1 +1,19 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+  console.log(idAudio);
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+
+  console.log(contador)
+  
+}
